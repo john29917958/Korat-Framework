e865076c8252f5c587613c11e26a10243afd4986
John Wang <john29917958@gmail.com>
2017-05-26 23:05:06 +0800

    Add DEMO code.

--- App/Program.cs
+++ App/Program.cs
@@ -1,4 +1,9 @@
-﻿using Ncu.Oolab.Korat.Library;
+﻿using System;
+using KoratFramework;
+using KoratFramework.Behaviors.Browsers.Chrome;
+using KoratFramework.Pickers.Behaviors.Browser;
+using KoratFramework.Pickers.Behaviors.Os;
+using Ncu.Oolab.Korat.Library;
 
 namespace App
 {
@@ -7,6 +12,19 @@ namespace App
         static void Main(string[] args)
         {
             Korat korat = new Korat();
+            BehaviorPool pool = new BehaviorPool();
+            pool.Add(new WinBehaviorsPicker(korat, pool).Pick("7"));
+            pool.Add(new ChromeBehaviorsPicker(korat, pool).Pick("60.0"));
+            ChromeBehaviors chrome = pool.Request<ChromeBehaviors>();
+            if (chrome == null)
+            {
+                Console.WriteLine("No such behaviors.");
+            }
+            else
+            {
+                chrome.FocusUrlBar();
+            }
+            Console.Read();
         }
     }
 }
